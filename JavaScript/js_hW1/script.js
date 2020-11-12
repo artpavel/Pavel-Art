884ba4d8e71b1104b3514b27c2c7c8cb3dca6cbd
Ivanfrankevych <32160039+Ivanfrankevych@users.noreply.github.com>
2020-11-12 03:56:16 +0200

    ф

--- JavaScript/js_hW1/script.js
+++ JavaScript/js_hW1/script.js
@@ -1,23 +1,23 @@
 // Завдання 1. Сума числа
 function SumNumber(n) {
-   let sum = 0;
-   while (n > 0) {
-      sum += n % 10
-      n = Math.floor(n / 10)
-   }
-   return sum;
+    let sum = 0;
+    while (n > 0) {
+        sum += n % 10
+        n = Math.floor(n / 10)
+    }
+    return sum;
 }
 
-let a = SumNumber(5555); 
+let a = SumNumber(1234);
 console.log('Задання 1. Сума цифр >>>>>  ' + a);
 
 // Завдання 2. Середнє арифметичне масиву
 function Average(arr) {
-   let sum = 0;
-   for (let i = 0; i < arr.length; i++) {
-      sum += arr[i];
-   }
-   return sum / arr.length;
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
 }
 
 let b = Average([1, 2, 3, 4, 5]);
@@ -25,34 +25,34 @@ console.log('Задання 2. Середнє арифметичне >>>>>  ' + 
 
 // Завдання 3. Різниця між парними і непарними числами масиву
 function Difference(array) {
-   let sumPair = 0;
-   let sumUnpair = 0;
-   for (let i = 0; i < array.length; i++) {
-      if (array[i] % 2 == 0) {
-         sumPair += array[i];
-      } else {
-         sumUnpair += array[i]
-      }
-   }
-   return sumPair - sumUnpair;
+    let sumPair = 0;
+    let sumUnpair = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 == 0) {
+            sumPair += array[i];
+        } else {
+            sumUnpair += array[i]
+        }
+    }
+    return sumPair - sumUnpair;
 }
 
 let c = Difference([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 console.log('Задання 3. Різниця між масивами >>>>>  ' + c);
 
 // Завдання 4. Порівняння масивів
-function CompareArrays(arr1, arr2){
-   let likeArray =  [];
-   for (let i = 0; i < arr1.length; i++) {
-      for (let j = 0; j < arr2.length; j++) {
-         if (arr1[i] == arr2[j]) {
-            likeArray.push(arr1[i]);
-         }         
-      }      
-   }
-   return likeArray;
+function CompareArrays(arr1, arr2) {
+    let likeArray = [];
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr2.length; j++) {
+            if (arr1[i] == arr2[j]) {
+                likeArray.push(arr1[i]);
+            }
+        }
+    }
+    return likeArray;
 }
 
-let d = CompareArrays([10, 25, 44, 32, 76, 99, 123] , [123, 15, 32, 55, 48, 76, 99]);
+let d = CompareArrays([10, 25, 44, 32, 76, 99, 123], [123, 15, 32, 55, 48, 76, 99]);
 console.log('Завдання 4. Новий масив >>>>>');
 console.log(d);
