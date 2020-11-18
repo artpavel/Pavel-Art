727918d7233c924fc5cf59a3e1a23048bfdd8bd0
artpavel <artpavel@ukr.net>
2020-11-18 18:32:08 +0200

    change WebBooks

--- WebBooks/script.js
+++ WebBooks/script.js
@@ -1,6 +1,33 @@
-let burger = document.getElementsByClassName('burger')[0];
-burger.addEventListener('click', function () {
-    burger.classList.toggle('active');
-    let menu = document.getElementsByTagName('header')[0].getElementsByTagName('ul')[0];
-    menu.classList.toggle('active');
+$('.burger').on('click', function () {
+    $(this).toggleClass('active');
+    $('ul').toggleClass('active');
+});
+
+
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: '<span class="next"></span>',
+    prevArrow: '<span class="prev"></span>',
+    responsive: [
+        {
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 700,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
 });
