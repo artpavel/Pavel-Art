ebf608a2c7cd01a17906dcae302eae9fae1c86d2
artpavel <artpavel@ukr.net>
2020-12-04 17:53:56 +0200

    add localhost

---
+++ localhost/script.js
@@ -0,0 +1,44 @@
+$('.burger').on('click', function () {
+    $(this).toggleClass('active');
+    $('ul').toggleClass('active');
+});
+
+
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: '<span class="next"></span>',
+    prevArrow: '<span class="prev"></span>',
+    responsive: [
+        {
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 700,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
+
+function Myscroll(a) {
+    let element = $(document).find(a);
+    $('html,body').animate({
+        scrollTop: element.offset().top}, 1000);
+ }
+ $('ul a').on('click', function (e) {
+    e.preventDefault();
+    let link = $(this).attr('href');
+    Myscroll(link);
+ });
