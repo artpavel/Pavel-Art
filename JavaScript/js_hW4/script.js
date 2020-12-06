3b7c0a6fd20a006f1d5905691756308131c52167
Ivanfrankevych <32160039+Ivanfrankevych@users.noreply.github.com>
2020-12-06 17:17:41 +0200

    Update script.js

--- JavaScript/js_hW4/script.js
+++ JavaScript/js_hW4/script.js
@@ -1,7 +1,10 @@
 let product = [];
 
 
-$('button').on('click', function(){
+$('button').on('click', function () {
+    if (localStorage.getItem('products') != null) {
+        product = JSON.parse(localStorage.getItem('products'));
+    }
     let str = $(this).parent('.product').html();
     product.push(str);
     let prodSet = [...new Set(product)];
@@ -9,14 +12,13 @@ $('button').on('click', function(){
     localStorage.setItem('products', myJSON);
 });
 
-$('.get_cart').on('click', function(){
-   let str = JSON.parse( localStorage.getItem('products') );
-   $('.show-cart').html(str);
-   let remove = $('.show-cart').find('button');
-   $('<button class="remove">Видалити товар</button>').replaceAll(remove);
-   $('.remove').append('<input type="number" class="count" value="1">');
-   console.log(str);
+$('.get_cart').on('click', function () {
+    let str = JSON.parse(localStorage.getItem('products'));
+    for(let i=0;i<str.length;i++){
+        $('.show-cart').append('<div class="product">'+str[i]+'</div>')
+    }
+    let remove = $('.show-cart').find('button');
+    $('<button class="remove">Видалити товар</button>').replaceAll(remove);
+    $('.show-cart .product').append('<input type="number" class="count" value="1">');
+    console.log(str);
 });
-
-
-
