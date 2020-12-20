0e1239318de0ae7e8ce1d5d0d9038e2dc17df8db
artpavel <artpavel@ukr.net>
2020-12-20 11:28:25 +0200

    change domains

---
+++ domains/WebBooks/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <a href="#top" class="logo">
+                <img src="images/image%2013.jpg" alt="">
+            </a>
+            <ul>
+                <li><a href="#top">Главная</a></li>
+                <li><a href="#aboutUs">Обо мне</a></li>
+                <li><a href="#1">Книги</a></li>
+                <li><a href="#new">Новости</a></li>
+                <li><a href="#testi">Отзывы</a></li>
+                <li><a href="#footer">Контакты</a></li>
+            </ul>
+            <a href="#" class="transparent_button">
+                Войти
+            </a>
+            <div class="burger">
+                <span></span>
+            </div>
+        </div>
+    </header>
