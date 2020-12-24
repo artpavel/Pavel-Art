dfd978bf2f2231fb4e7c94d81e292faa7c8a5f8b
artpavel <artpavel@ukr.net>
2020-12-24 19:27:16 +0200

    add php

---
+++ domains/HomeWorkPhp/class.php
@@ -0,0 +1,152 @@
+<?php
+
+abstract class Product
+{
+    abstract function  getNameOfProduct();
+    abstract function  calculateOfPrice();
+
+    function Display()
+    {
+        print 'Title product: ' . $this->getNameOfProduct() . '<br>';
+        print 'Price product: ' . $this->calculateOfPrice();
+    }
+}
+
+// Клас штани
+class Pants extends Product
+{
+    // Поля класу
+    private $title;
+    private $length;
+    private $costOfMaterial;
+
+
+    // Гетери і сетери
+    function getTitle()
+    {
+        return $this->title;
+    }
+
+    function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    function getLength()
+    {
+        return $this->length;
+    }
+
+    function setLength($length)
+    {
+        $this->length = $length;
+    }
+
+    function getCostOfMaterial()
+    {
+        return $this->costOfMaterial;
+    }
+
+    function setCostOfMaterial($costOfMaterial)
+    {
+        $this->costOfMaterial = $costOfMaterial;
+    }
+
+    // Конструктор
+    function __construct($title, $length, $costOfMaterial)
+    {
+        $this->title = $title;
+        $this->length = $length;
+        $this->costOfMaterial = $costOfMaterial;
+    }
+
+    // Абстрактні методи
+    function getNameOfProduct()
+    {
+        return $this->title;
+    }
+
+    function calculateOfPrice()
+    {
+        return $this->length * $this->costOfMaterial . ' uan <br>';
+    }
+}
+
+// Клас сорочка
+class Shirt extends Product
+{
+    // Поля класу
+    private $title;
+    private $size;
+    private $costOfMaterial;
+    private $discount;
+
+
+
+    // Гетери і сетери
+    function getTitle()
+    {
+        return $this->title;
+    }
+
+    function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    function getSize()
+    {
+        return $this->size;
+    }
+
+    function setLength($size)
+    {
+        $this->size = $size;
+    }
+
+    function getCostOfMaterial()
+    {
+        return $this->costOfMaterial;
+    }
+
+    function setCostOfMaterial($costOfMaterial)
+    {
+        $this->costOfMaterial = $costOfMaterial;
+    }
+
+    function getDiscount()
+    {
+        return $this->discount;
+    }
+
+    function setDiscount($discount)
+    {
+        $this->discount = $discount;
+    }
+
+    // Конструктор
+    function __construct($title, $size, $costOfMaterial, $discount)
+    {
+        $this->title = $title;
+        $this->size = $size;
+        $this->costOfMaterial = $costOfMaterial;
+        $this->discount = $discount;
+    }
+
+    // Обчислення знижки
+    function calculateOfDiscount()
+    {
+        return $this->size * $this->costOfMaterial / 100 * $this->discount;
+    }
+
+    // Абстрактні методи
+    function getNameOfProduct()
+    {
+        return $this->title;
+    }
+
+    function calculateOfPrice()
+    {
+        return $this->size * $this->costOfMaterial - $this->calculateOfDiscount() . ' uan <br>';
+    }
+}
---
+++ domains/HomeWorkPhp/index.php
@@ -0,0 +1,63 @@
+<!-- Це ми підключили на PHP-файл -->
+<?php
+include 'class.php'
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    $myP1 = new Pants('jeans1', 2, 300);
+    $myP2 = new Pants('jeans2', 4, 100);
+    $myP3 = new Pants('jeans3', 6, 200);
+
+    $myS1 = new Shirt('style1', 22, 150, 10);
+    $myS2 = new Shirt('style2', 32, 100, 30);
+    $myS3 = new Shirt('style3', 42, 550, 15);
+
+    $myArr = [$myP2, $myS1, $myP3, $myS3, $myP1, $myS2];
+
+    foreach ($myArr as $item) {
+        $item->display() . '<br>';
+        echo '<br>';
+    }
+
+    echo '<br>';
+    echo '<h2>Новий масив</h2>';
+    echo '<br>';
+
+    foreach ($myArr as $item) {
+        if (in_array($item->getDiscount, $myArr)) {
+            continue;
+        } else {
+            $item->setCostOfMaterial($item->getCostOfMaterial() + 50);
+        }
+    }
+
+    foreach ($myArr as $item) {
+        $item->display() . '<br>';
+        echo '<br>';
+    }
+
+
+
+
+    ?>
+
+
+
+
+
+
+</body>
+
+</html>
